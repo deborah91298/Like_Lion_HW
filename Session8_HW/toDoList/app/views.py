4f0ae4947dce5cebca86153fdafe8dfc08bf9629
LionLin <deborah91298@hotmail.com>
2021-05-13 17:42:01 +0900

    session8_hw

---
+++ Session8_HW/toDoList/app/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from .models import List
+
+# Create your views here.
+def home(request):
+    lists = List.objects.all().order_by('deadline')
+
+    return render(request, 'home.html', {'lists' : lists})
+
+def todo(request):
+    lists = List.objects.all().order_by('deadline')
+
+    return render(request, 'todo.html', {'lists' : lists})
+
+def new(request):
+    if request.method == 'POST':
+        new_list = List.objects.create(
+            title = request.POST['title'],
+            content = request.POST['content'],
+            deadline = request.POST['deadline']
+        )
+        return redirect('detail', new_list.pk)
+
+    return render(request, 'new.html')
+
+def detail(request, list_pk):
+    list = List.objects.get(pk=list_pk)
+
+    return render(request, 'detail.html', {'list': list})
+
+def edit(request, list_pk):
+    list = List.objects.get(pk=list_pk)
+
+    if request.method == 'POST':
+        List.objects.filter(pk=list_pk).update(
+            title = request.POST['title'],
+            content = request.POST['content'],
+            deadline = request.POST['deadline']
+        )
+        return redirect('detail', list_pk)
+    
+    return render(request, 'edit.html', {'list': list})
+
+def delete(request, list_pk):
+    list = List.objects.get(pk=list_pk)
+    list.delete()
+
+    return redirect('home')
